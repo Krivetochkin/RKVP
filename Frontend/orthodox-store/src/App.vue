91584bbe3bc1b6ce4740e54ffb0d02f5eabdc466
Krivetochkin <siremartin5500@gmail.com>
2026-04-10 10:26:09 +0300

    Create App.vue
    Добавил App

---
+++ Frontend/orthodox-store/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="app">
+    <Header />
+    <main class="container">
+      <router-view />
+    </main>
+  </div>
+</template>
+<script setup>
+import Header from './components/Header.vue';
+</script>
